9462f782575fac4ca55767e15c96ddf2fa546e9e
Shrihari Asokan <shrihariasokan@Shriharis-MacBook-Pro.local>
2024-06-25 00:16:22 -0600

    webpack installed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //import  './style.css'
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import * as THREE from 'three'; //'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import ThreeGlobe from 'https://esm.sh/three-globe@2.30.0';
 import getStarfield from './getStarfield.js';
